7ee6101754d9831c46271769d47c1e9c32ebb601
JonsonsBabby <sanvik258@gmail.com>
2022-11-28 16:31:43 +0200

    v1.0.30

--- js/index.js
+++ js/index.js
@@ -3,11 +3,3 @@ const nav = document.querySelector('.header__navigation');
 const navBtnClose = document.querySelector('.btn__close');
 navBtnClose.addEventListener('click', ()=> {nav.style.display = 'none'});
 navBtnOpen.addEventListener('click', ()=>{nav.style.display ='flex'});
-
-const left = document.querySelector(".slider__arrow--left")
-const right = document.querySelector(".slider__arrow--right")
-const slider = document.querySelector(".slider__frame")
-
-right.addEventListener("click",()=>{
-    console.log("clicked")
-})
